91657996fe80a3cece340626005d0da1a5bba7e4
Hunter Heston <huntersamuelheston@gmail.com>
2023-02-26 10:56:51 -0800

    Added a navigation component

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Navigation from "./navigation";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -13,6 +14,7 @@ export default function Layout({ children }: LayoutProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navigation />
       {children}
     </>
   );
