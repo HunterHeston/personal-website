d7c437b6fac5571bb9eff9158266da776df93714
Hunter Heston <huntersamuelheston@gmail.com>
2023-02-26 20:23:50 -0800

    Colors defined

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      // https://www.jimraptis.com/tools/color-pairs
+      background: "#212529",
+      primary: "#dae8df",
+      accent: "#81b29a",
+    },
   },
   plugins: [],
 };
