f7c646b936d2d0e6cf7aa98f78d282db49a2bbca
Hunter Heston <huntersamuelheston@gmail.com>
2023-02-26 19:14:39 -0800

    Remove water css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import Layout from "@/components/layout";
 import "@/styles/globals.css";
-import "@/styles/water.css";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
