6611f84fb54b2b72fa930cedceaabe1e71766658
Hunter Heston <huntersamuelheston@gmail.com>
2023-04-03 08:51:48 -0700

    Restricting my site to 3 pages.

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -4,18 +4,9 @@ export default function Navigation() {
   return (
     <>
       <nav>
-        <ul className="flex  w-screen">
+        <ul className="flex w-screen justify-around">
           <li>
             <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/work">Work</Link>
-          </li>
-          <li>
-            <Link href="/blog">News Letter</Link>
           </li>
           <li>
             <Link href="/blog">Blog</Link>
