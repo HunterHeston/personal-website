268b3601c62490d06443388033304a1c63950418
Hunter Heston <huntersamuelheston@gmail.com>
2023-04-04 08:45:43 -0700

    Apply inter font to all pages.

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,4 @@
+import { Inter } from "@next/font/google";
 import Head from "next/head";
 import Navigation from "./navigation";
 
@@ -5,9 +6,11 @@ type LayoutProps = {
   children: React.ReactNode;
 };
 
+const inter = Inter({ subsets: ["latin"] });
+
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="dark:bg-background dark:text-primary">
+    <div className={"dark:bg-background dark:text-primary " + inter.className}>
       <Head>
         <title>Hunter Heston</title>
         <meta name="description" content="Hunter Heston's personal website." />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
-import { Inter } from "@next/font/google";
 import Image from "next/image";
 import Link from "next/link";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
