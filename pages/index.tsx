d5e6d6ab2792cb727597651aece715762f36ede2
Hunter Heston <huntersamuelheston@gmail.com>
2023-04-06 10:19:18 -0700

    Removed noise from the home page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,54 +5,38 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <H1>Hunter Heston</H1>
-      <h2>Software Engineer. Web Developer. Small Business Owner</h2>
-      <Image
-        className="rounded-full"
-        alt="hunter heston's head shot"
-        src="/hunterheston.jpeg"
-        width={100}
-        height={100}
-      />
-      <hr />
+      <section>
+        <H1>Hunter Heston</H1>
+        <h2 className="leading-loose italic">
+          Software Engineer. Web Developer. Small Business Owner
+        </h2>
+        <Image
+          className="rounded-full my-5"
+          alt="hunter heston's head shot"
+          src="/hunterheston.jpeg"
+          width={100}
+          height={100}
+        />
+      </section>
+      {/* <hr /> */}
       <section>
         <H2>About Me</H2>
-        <p>
-          Hey! I&apos;m Hunter, a software engineer and small business owner
-          living in San Francisco.
+        <p className="my-5">
+          Hey! I&apos;m <span className="font-bold">Hunter</span>, a software
+          engineer and small business owner living in San Francisco.
         </p>
-        <p>
+        <p className="my-5">
           For the past 6 years I&apos;ve been building software at companies
-          like Google and Lockheed Martin. Now I build software for clients!
+          like <span className="font-bold">Google and Lockheed Martin</span>.
         </p>
-        <p>
+        <p className="my-5">
           My strengths are backend service development in Go and NodeJS.
           I&apos;m skilled at building frontends with ReactJS or vanilla
           HTML/CSS/JavaScript.
         </p>
-        <p>I am passionate about coding, writing and personal growth.</p>
-      </section>
-      <hr />
-      <section>
-        <H2>Contact</H2>
-        <form action="submit">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" />
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
-          <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" />
-          <button type="submit">Send</button>
-        </form>
-      </section>
-      <hr />
-      <section>
-        <H2>Join My Newsletter</H2>
-        <form action="submit">
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
-          <button type="submit">Subscribe</button>
-        </form>
+        <p className="my-5">
+          I am passionate about coding, writing and personal growth.
+        </p>
       </section>
     </>
   );
