bc3241cfbe99f12d332e11717055351aa5599bb6
Hunter Heston <huntersamuelheston@gmail.com>
2023-04-06 09:02:08 -0700

    Correct some placeholder typos

--- pages/work.tsx
+++ pages/work.tsx
@@ -19,23 +19,23 @@ export default function Contact() {
       </div>
       <div>
         <Image
-          alt="screenshot of project 1"
+          alt="screenshot of project 2"
           src="https://placehold.co/600x400"
           width={600}
           height={400}
         />
-        <h3>Project 1</h3>
+        <h3>Project 2</h3>
         <Link href="/project2">Fools Errand</Link> |{" "}
         <Link href="/project2">Code</Link>
       </div>
       <div>
         <Image
-          alt="screenshot of project 1"
+          alt="screenshot of project 3"
           src="https://placehold.co/600x400"
           width={600}
           height={400}
         />
-        <h3>Project 1</h3>
+        <h3>Project 3</h3>
         <Link href="/project3">Benchmark Climbing Gym</Link> |{" "}
         <Link href="/project3">Code</Link>
       </div>
