345c7d3a5054cdece59521acae4c78c05aec1414
Hunter Heston <huntersamuelheston@gmail.com>
2023-04-06 11:24:08 -0700

    Bolded more points in the text and increased line height.

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,20 +21,24 @@ export default function Home() {
       {/* <hr /> */}
       <section>
         <H2>About Me</H2>
-        <p className="my-5">
+        <p className="my-5 leading-relaxed">
           Hey! I&apos;m <span className="font-bold">Hunter</span>, a software
           engineer and small business owner living in San Francisco.
         </p>
-        <p className="my-5">
+        <p className="my-5 leading-relaxed">
           For the past 6 years I&apos;ve been building software at companies
           like <span className="font-bold">Google and Lockheed Martin</span>.
         </p>
-        <p className="my-5">
-          My strengths are backend service development in Go and NodeJS.
-          I&apos;m skilled at building frontends with ReactJS or vanilla
-          HTML/CSS/JavaScript.
+        <p className="my-5 leading-relaxed">
+          My strengths are{" "}
+          <span className="font-bold">backend service development</span> in{" "}
+          <span className="font-bold">Go</span> and
+          <span className="font-bold">NodeJS</span>. I&apos;m skilled at
+          building <span className="font-bold">frontends</span> with{" "}
+          <span className="font-bold">ReactJS</span> or vanilla{" "}
+          <span className="font-bold">HTML/CSS/JavaScript</span>.
         </p>
-        <p className="my-5">
+        <p className="my-5 leading-relaxed">
           I am passionate about coding, writing and personal growth.
         </p>
       </section>
