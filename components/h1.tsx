eb92658fbfa6dd2e9b3d9e24cd3325418d7ce782
Hunter Heston <huntersamuelheston@gmail.com>
2023-04-06 11:51:44 -0700

    Decrease letter spacing on titles.

--- components/h1.tsx
+++ components/h1.tsx
@@ -6,7 +6,7 @@ type H1Props = {
 // H1 component returns an h1 element with a set of tailwind classes.
 export default function H1({ children }: H1Props) {
   return (
-    <h1 className="leading-loose text-4xl font-semibold text-accent">
+    <h1 className="leading-loose text-4xl font-semibold text-accent tracking-tight">
       {children}
     </h1>
   );
