337898777fef3c3b097edc6a06e5b539b7ac476e
Hunter Heston <huntersamuelheston@gmail.com>
2023-04-07 10:25:23 -0700

    Project card styled with icons and links to code/site.

--- components/projectCard.tsx
+++ components/projectCard.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
+import ArrowUpRightIcon from "./icons/arrowUpRight";
+import WebIcon from "./icons/web";
+import GitHubIcon from "./icons/github";
 
 // ProjectCard Props
 interface ProjectCardProps {
@@ -17,22 +20,47 @@ export default function ProjectCard({
 }: ProjectCardProps) {
   return (
     <>
-      <div>
+      <div className="mb-10">
         <Image
+          className="rounded-lg"
           alt="screenshot of project interface"
           src={image}
           width={600}
           height={400}
         />
-        <h2>{title}</h2>
-        <Link href={projectLink} target="_blank">
-          Live Site
-        </Link>{" "}
-        |{" "}
-        <Link href={codeLink} target="_blank">
-          Code
-        </Link>
+        <h2 className="text-2xl py-5">{title}</h2>
+        <div className="flex">
+          <LinkButton href={projectLink}>
+            <WebIcon className="mr-2" />
+            View
+          </LinkButton>
+          <LinkButton href={codeLink}>
+            <GitHubIcon className="mr-2" />
+            Code
+          </LinkButton>
+        </div>
       </div>
+    </>
+  );
+}
+
+// LinkButton Props
+interface LinkButtonProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+function LinkButton({ href, children }: LinkButtonProps) {
+  return (
+    <>
+      <Link
+        href={href}
+        target="_blank"
+        className="flex border  border-primary dark:border-darker rounded-lg mr-4 py-2 px-4 items-center justify-center hover:bg-darker hover:text-white transition duration-300 ease-in-out"
+      >
+        {children}
+        <ArrowUpRightIcon className="ml-2" />
+      </Link>
     </>
   );
 }
--- pages/work.tsx
+++ pages/work.tsx
@@ -1,32 +1,32 @@
 import H1 from "@/components/h1";
 import P from "@/components/p";
 import ProjectCard from "@/components/projectCard";
-import Image from "next/image";
-import Link from "next/link";
 
 export default function Contact() {
   return (
     <>
       <H1>Work</H1>
       <P>An overview of some of the projects I&apos;ve worked on.</P>
-      <ProjectCard
-        title="Bean Bag Cafe"
-        image="/600x400.jpeg"
-        projectLink="https://beanbag.cafe"
-        codeLink="https://github.com/hunterheston"
-      />
-      <ProjectCard
-        title="Fools Errand"
-        image="/600x400.jpeg"
-        projectLink="https://foolserrandsf.com"
-        codeLink="https://github.com/hunterheston"
-      />
-      <ProjectCard
-        title="Benchmark Climbing Gym"
-        image="/600x400.jpeg"
-        projectLink="https://benchmarkclimbing.com"
-        codeLink="https://github.com/hunterheston"
-      />
+      <div className="sm:flex flex-wrap">
+        <ProjectCard
+          title="Bean Bag Cafe"
+          image="/600x400.jpeg"
+          projectLink="https://beanbag.cafe"
+          codeLink="https://github.com/hunterheston"
+        />
+        <ProjectCard
+          title="Fools Errand"
+          image="/600x400.jpeg"
+          projectLink="https://foolserrandsf.com"
+          codeLink="https://github.com/hunterheston"
+        />
+        <ProjectCard
+          title="Benchmark Climbing Gym"
+          image="/600x400.jpeg"
+          projectLink="https://benchmarkclimbing.com"
+          codeLink="https://github.com/hunterheston"
+        />
+      </div>
     </>
   );
 }
