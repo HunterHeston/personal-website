1b337120d78feb56c96a739d177d4876079a7260
Hunter Heston <huntersamuelheston@gmail.com>
2023-04-10 09:40:26 -0700

    Style contact form.

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,17 +1,55 @@
 import H1 from "@/components/h1";
+import H2 from "@/components/h2";
 
 export default function Contact() {
   return (
     <>
       <H1>Contact</H1>
-      <form action="submit">
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-        <label htmlFor="message">Message</label>
-        <textarea name="message" id="message" />
-        <button type="submit">Send</button>
+      <p className="text-xl mb-4">Lets work together!</p>
+      <form className="flex flex-col items-start" action="submit">
+        <label
+          className="flex flex-col leading-relaxed w-full text-xl mb-3"
+          htmlFor="name"
+        >
+          Name
+          <input
+            className="border rounded-md p-4 leading-normal text-xl"
+            type="text"
+            name="name"
+            id="name"
+          />
+        </label>
+        <label
+          className="flex flex-col leading-relaxed w-full text-xl mb-3"
+          htmlFor="email"
+        >
+          Email
+          <input
+            className="border rounded-md p-4 leading-normal text-xl"
+            required
+            type="email"
+            name="email"
+            id="email"
+          />
+        </label>
+        <label
+          className="flex flex-col leading-relaxed w-full text-xl mb-3"
+          htmlFor="message"
+        >
+          Message
+          <textarea
+            className="border rounded-md p-4 leading-normal text-xl"
+            required
+            name="message"
+            id="message"
+          />
+        </label>
+        <button
+          className="w-full p-4 mt-4 bg-background text-primary rounded-md"
+          type="submit"
+        >
+          Send
+        </button>
       </form>
     </>
   );
