f75eddc264fb2b08e1fd20592e8ff2560b3a21e3
Hunter Heston <huntersamuelheston@gmail.com>
2023-04-10 16:59:02 -0700

    Set custom selection color for text

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="dark:bg-background dark:text-primary">
+      <body className="dark:bg-background dark:text-primary selection:bg-accent">
         <Main />
         <NextScript />
       </body>
