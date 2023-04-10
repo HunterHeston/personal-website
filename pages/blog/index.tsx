7f25c6c389fb09f237396cebb425c40ef3c1d8ac
Hunter Heston <huntersamuelheston@gmail.com>
2023-04-10 14:22:55 -0700

    Generate blog listing at build time.

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,6 +1,16 @@
 import H1 from "@/components/h1";
+import { Blog, listBlogs } from "@/lib/blogs";
+import Link from "next/link";
 
-export default function Blog() {
+// Blog Props
+interface BlogProps {
+  blogs: Blog[];
+}
+
+export default function BlogListing({ blogs }: BlogProps) {
+  for (const blog of blogs) {
+    console.log(blog);
+  }
   return (
     <>
       <H1>Blog</H1>
@@ -10,7 +20,45 @@ export default function Blog() {
         <input type="email" name="email" id="email" />
         <button type="submit">Subscribe</button>
       </form>
-      <p>Coming soon!</p>
+      <ul>
+        {blogs.map((blog) => (
+          <li key={blog.title}>
+            <Link href={`/blog${blog.title}`}>{blog.title}</Link>
+            <p>
+              {blog.date} <span>{blog.readingTime} min read</span>
+            </p>
+          </li>
+        ))}
+      </ul>
     </>
   );
+}
+
+/**
+ * Generate static props for the blog listing page
+ * @returns {Blog[] | Error}
+ */
+export async function getStaticProps() {
+  let blogs: Blog[] = [];
+
+  try {
+    const result = await listBlogs();
+    if (result instanceof Error) {
+      console.error(`static generation of blog props failed: ${result}`);
+      throw result;
+    }
+
+    blogs = result;
+  } catch (e) {
+    console.error(
+      `static generation of blog props threw unexpected error: ${e}`
+    );
+    throw e;
+  }
+
+  return {
+    props: {
+      blogs,
+    },
+  };
 }
