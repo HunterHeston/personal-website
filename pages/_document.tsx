4a4e2661e56aad959d1f5756a58a737cafd1b27a
Hunter Heston <huntersamuelheston@gmail.com>
2023-04-11 12:08:08 -0700

    hight contrast when text is highlighted

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="dark:bg-background dark:text-primary selection:bg-accent">
+      <body className="dark:bg-background dark:text-primary selection:bg-accent selection:text-background">
         <Main />
         <NextScript />
       </body>
