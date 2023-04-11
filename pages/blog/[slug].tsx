b5d179682e551cdc1057eb9d7cb2b6725dcff75a
Hunter Heston <huntersamuelheston@gmail.com>
2023-04-11 11:43:32 -0700

    Blog content styling is pretty solid. 80/20

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -7,6 +7,8 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import remarkGfm from "remark-gfm";
 
+import style from "./slug.module.css";
+
 // Blog Props
 interface BlogProps {
   blog: Blog;
@@ -15,36 +17,76 @@ interface BlogProps {
 export default function BlogPost({ blog }: BlogProps) {
   return (
     <>
-      <ReactMarkdown
-        remarkPlugins={[remarkGfm]}
-        components={{
-          h1: ({ node, ...props }) => <H1 {...props} />,
-          h2: ({ node, ...props }) => <H2 {...props} />,
-          h3: ({ node, ...props }) => <h3 {...props} />,
-          h4: ({ node, ...props }) => <h4 {...props} />,
-          h5: ({ node, ...props }) => <h5 {...props} />,
-          h6: ({ node, ...props }) => <h6 {...props} />,
-          p: ({ node, ...props }) => <P {...props} />,
-          a: ({ node, ...props }) => <a {...props} />,
-          blockquote: ({ node, ...props }) => <blockquote {...props} />,
-          br: ({ node, ...props }) => <br {...props} />,
-          em: ({ node, ...props }) => <em {...props} />,
-          hr: ({ node, ...props }) => <hr {...props} />,
-          img: ({ node, ...props }) => <img {...props} />,
-          li: ({ node, ...props }) => <li {...props} />,
-          ol: ({ node, ...props }) => <ol {...props} />,
-          pre: ({ node, ...props }) => <pre {...props} />,
-          strong: ({ node, ...props }) => <strong {...props} />,
-          ul: ({ node, ...props }) => <ul {...props} />,
-          code: ({ node, inline, className, children, ...props }) => (
-            <Code inline={inline} className={className} {...props}>
-              {String(children)}
-            </Code>
-          ),
-        }}
-      >
-        {blog.content}
-      </ReactMarkdown>
+      <article className="text-md">
+        <ReactMarkdown
+          remarkPlugins={[remarkGfm]}
+          components={{
+            h1: ({ node, ...props }) => <H1 {...props} />,
+            h2: ({ node, ...props }) => <H2 {...props} />,
+            h3: ({ node, ...props }) => (
+              <h3 className="text-xl mb-2" {...props} />
+            ),
+            h4: ({ node, ...props }) => <h4 className="mb-2" {...props} />,
+            h5: ({ node, ...props }) => <h5 className="mb-2" {...props} />,
+            h6: ({ node, ...props }) => <h6 className="mb-2" {...props} />,
+            p: ({ node, ...props }) => (
+              <p className="leading-loose my-5" {...props} />
+            ),
+            a: ({ node, ...props }) => (
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="decoration-accent underline visited:text-accent "
+                {...props}
+              />
+            ),
+            blockquote: ({ node, ...props }) => (
+              <blockquote
+                className="border-l-4 border-l-neutral-600 rounded-sm pl-5 bg-neutral-200 dark:bg-darker"
+                {...props}
+              />
+            ),
+            br: ({ node, ...props }) => <br {...props} />,
+            em: ({ node, ...props }) => <em {...props} />,
+            hr: ({ node, ...props }) => <hr className="my-4" {...props} />,
+            img: ({ node, ...props }) => (
+              <img className="w-auto h-auto" {...props} />
+            ),
+            pre: ({ node, ...props }) => <pre className="" {...props} />,
+            strong: ({ node, ...props }) => <strong {...props} />,
+            ol: ({ node, ...props }) => (
+              <ol className="list-decimal my-5 pl-3" {...props} />
+            ),
+            ul: ({ node, ...props }) => (
+              <ul className="list-disc mb-5 mt-1 pl-3" {...props} />
+            ),
+            li: ({ node, ...props }) => <li {...props} />,
+            code: ({ node, inline, className, children, ...props }) => (
+              <Code inline={inline} className={className} {...props}>
+                {String(children)}
+              </Code>
+            ),
+            table: ({ node, ...props }) => (
+              <table className="table-auto w-full " {...props} />
+            ),
+            thead: ({ node, ...props }) => (
+              <thead className="border-b" {...props} />
+            ),
+            tbody: ({ node, ...props }) => <tbody {...props} />,
+            tr: ({ node, ...props }) => (
+              <tr className="even:text-accent" {...props} />
+            ),
+            th: ({ node, ...props }) => (
+              <th className="p-3 text-start" {...props} />
+            ),
+            td: ({ node, ...props }) => (
+              <td className="p-3 text-start" {...props} />
+            ),
+          }}
+        >
+          {blog.content}
+        </ReactMarkdown>
+      </article>
     </>
   );
 }
