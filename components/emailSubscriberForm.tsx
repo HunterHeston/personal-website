b46aeec42d7d42e84893c9b95683df81e2dd0a6d
Hunter Heston <huntersamuelheston@gmail.com>
2023-04-12 12:07:16 -0700

    Update frontend and api route to use new mailchimp library.

--- components/emailSubscriberForm.tsx
+++ components/emailSubscriberForm.tsx
@@ -1,7 +1,14 @@
 import { useState, FormEvent } from "react";
 
+enum SubStatus {
+  Success,
+  Error,
+  None,
+}
+
 export default function EmailForm() {
   const [email, setEmail] = useState("");
+  const [subState, setSubState] = useState(SubStatus.None);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -17,9 +24,10 @@ export default function EmailForm() {
 
       if (res.ok) {
         setEmail("");
-        console.log("Subscribed!");
+        setSubState(SubStatus.Success);
       } else {
-        console.error("Failed to subscribe.");
+        setEmail("");
+        setSubState(SubStatus.Error);
       }
     } catch (error) {
       console.log("Error occurred when send subscription request: ", error);
@@ -27,26 +35,51 @@ export default function EmailForm() {
   };
 
   return (
-    <form className="flex mb-10" onSubmit={handleSubmit}>
-      <label htmlFor="email" hidden>
-        Email
-      </label>
-      <input
-        className="p-2 w-72 border-l border-t border-b rounded-l-md rounded-r-none"
-        placeholder="example@gmail.com"
-        type="email"
-        name="email"
-        required
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+    <>
+      <form className="flex mb-10" onSubmit={handleSubmit}>
+        <label htmlFor="email" hidden>
+          Email
+        </label>
+        <input
+          className={
+            "p-2 w-72 border-l border-t border-b rounded-l-md rounded-r-none " +
+            getSubStateChangeClasses(subState)
+          }
+          placeholder={getPlaceHolderText(subState)}
+          type="email"
+          name="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-      <button
-        className="transition-all p-3 text-primary bg-background dark:bg-accent dark:text-background rounded-r-md hover:bg-darker hover:text-primary"
-        type="submit"
-      >
-        Subscribe
-      </button>
-    </form>
+        <button
+          className="transition-all p-3 text-primary bg-background dark:bg-accent dark:text-background rounded-r-md hover:bg-darker hover:text-primary"
+          type="submit"
+        >
+          Subscribe
+        </button>
+      </form>
+    </>
   );
+}
+
+function getSubStateChangeClasses(subState: SubStatus) {
+  if (subState === SubStatus.Success) {
+    return "bg-green-100";
+  }
+  if (subState === SubStatus.Error) {
+    return "bg-red-100";
+  }
+  return "";
+}
+
+function getPlaceHolderText(subState: SubStatus) {
+  if (subState === SubStatus.Success) {
+    return "Success! You're subscribed.";
+  }
+  if (subState === SubStatus.Error) {
+    return "Error! Please try again.";
+  }
+  return "example@gmail.com";
 }
--- pages/api/new-subscriber.ts
+++ pages/api/new-subscriber.ts
@@ -1,3 +1,4 @@
+import { SubscriptionResult, subscribeToBlog } from "@/lib/mailchimp";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
@@ -6,19 +7,26 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // only allow POST requests
   if (req.method !== "POST") {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method Not Allowed");
     return;
   }
 
+  // get the email and validate it
   const subscriberEmail: string = req.body["email"];
-
   if (!expression.test(subscriberEmail)) {
-    console.warn(`Invalid email address provided to signup form.`);
     res.status(400).json({ error: "Invalid email address provided" });
     return;
   }
 
-  res.status(200).json({ name: subscriberEmail });
+  // subscribe the user to the list using mailchimp API
+  const result: SubscriptionResult = await subscribeToBlog(subscriberEmail);
+  if (result.success === false) {
+    res.status(result.status as number).json({ error: result.message });
+    return;
+  }
+
+  res.status(200).json({ message: "successfully subscribed to email list" });
 }
