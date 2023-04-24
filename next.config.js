7b2a92f9bd845c48bee20915f9c0480efb97ea60
Hunter Heston <huntersamuelheston@gmail.com>
2023-04-24 08:38:12 -0700

    Redirect old notion home page to root.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   images: {
     domains: ["placehold.co"],
   },
+  redirects: async () => {
+    return [
+      {
+        source: "/b5fc0bf9d8d4409d95281c9c316d47f4",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
