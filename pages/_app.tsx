c9eea67944be530bceb42e62cae277c1e78155dc
Hunter Heston <huntersamuelheston@gmail.com>
2023-04-27 08:57:33 -0700

    Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
+import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import Layout from "@/components/layout";
 import "@/styles/globals.css";
-
-import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics></Analytics>
     </Layout>
   );
 }
